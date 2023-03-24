3fb367dcb256a9568aa5362bd25b419ebc2ae09d
unknown <mouni.shalaby@gmail.com>
2023-03-24 17:49:34 +0300

    Fetching a single item

--- pages/mouni/[id].js
+++ pages/mouni/[id].js
@@ -1,26 +1,39 @@
-export const getStatcPaths=async()=>{
+export const getStaticPaths  = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
-  const paths=data.map(mouni=>{
+  const paths = data.map(mouni => {
     return {
-      params:{id: mounies.id.toString()}
-    }
-  })
+      params: { id: mouni.id.toString() },
+    };
+  });
 
-return {
-  paths,
-  fallback:false
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+  const data = await res.json();
+  
+  return {
+  props :{mouni:data}
 }
-}
+};
 
-const Details=()=> {
 
+const Details = ({mouni}) => {
   return (
     <div>
-        <h1>Details Page</h1>
+      <h1>{mouni.name}</h1>
+      <p>{mouni.email}</p>
+      <p>{mouni.website}</p>
+      <p>{mouni.address.city}</p>
     </div>
-  )
-}
+  );
+};
 
 export default Details;
